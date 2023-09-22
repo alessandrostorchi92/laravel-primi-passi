c6e5393d8041bd60243157f9320b77c619c75651
alessandrostorchi92 <storchi.alle@gmail.com>
2023-09-22 17:05:18 +0200

    Stampa Hello Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Inizio a muovere i primi passi con questo fantastico framework che è Laravel">
+    <title>Primi passi con Laravel</title>
+
+    <!-- Framework Bootstrap css  -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <!-- Font Awesome CDN  -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+</head>
+
+<body>
+
+    <main>
+    
+        <div class="container mt-5">
+
+            <div class="text-center mt-4">
+                <h1>Hello Laravel</h1>
+            </div>
+
+        </div>
+    
+    </main>
+    
+    
+    </body>
+    
+    
+    </html>
